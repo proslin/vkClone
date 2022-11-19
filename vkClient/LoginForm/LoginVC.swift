5123332cef8db8d56e1f9f8f69331771855f886e
proslin <linchik@mail.ru>
2022-11-19 16:02:45 +0300

    final

--- vkClient/LoginForm/LoginVC.swift
+++ vkClient/LoginForm/LoginVC.swift
@@ -40,20 +40,21 @@ class LoginVC: UIViewController {
 extension LoginVC: WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse:
                  WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-    guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment else {
-    decisionHandler(.allow)
-    return
-    }
-    let params = fragment
-    .components(separatedBy: "&")
-    .map { $0.components(separatedBy: "=") } .reduce([String: String]()) { result, param in
-    var dict = result
-        let key = param[0]
-        let value = param[1]
-        dict[key] = value
-        return dict
-    }
-    let token = params["access_token"]
+        guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment else {
+            decisionHandler(.allow)
+            return
+        }
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+            }
+        let token = params["access_token"]
         guard token != nil else { return }
         Session.shared.token = token ?? " "
         let tabBarController = (self.storyboard?.instantiateViewController(withIdentifier: "TabBarControllerKey"))!
